a13a502501608a8d7021624ebb7ee74f2bdaf355
Roman Ostapchuk <pamwdp@yandex.ru>
2020-05-24 02:26:52 +0300

    v0.0.1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,10 @@
 /* eslint-disable consistent-return */
 const jwt = require('jsonwebtoken');
+const KEY = require('../config');
 const Unauthorize = require('../errors/unautorize');
 
 module.exports.auth = (req, res, next) => {
   const token = req.cookies.jwt;
-  const { NODE_ENV, JWT_SECRET } = process.env;
   let payload;
   const UnauthorizeError = new Unauthorize('Нужно войти в систему');
 
@@ -14,7 +14,7 @@ module.exports.auth = (req, res, next) => {
   }
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'SOME-SECRET-KEY');
+    payload = jwt.verify(token, KEY);
   } catch (err) {
     return res.status(UnauthorizeError.statusCode || 500)
       .send({ message: UnauthorizeError.message });
