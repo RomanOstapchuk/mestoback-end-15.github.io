4c30ab96c792c74d682dc6cb47b28b6f334406ad
Roman Ostapchuk <pamwdp@yandex.ru>
2020-05-24 00:10:31 +0300

    add ERR LOG CELEB

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,22 @@
 const cardRouter = require('express').Router();
-
+const { Joi, celebrate } = require('celebrate');
 const { getCards, createCard, deleteCard } = require('../controllers/cards');
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', createCard);
-cardRouter.delete('/:cardId', deleteCard);
+cardRouter.post('/',
+celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().uri(),
+  }),
+}),
+createCard);
+cardRouter.delete('/:cardId',
+celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24),
+  }),
+}),
+deleteCard);
 
 module.exports = cardRouter;
